412bde9288b857f465e0788858d81aefe53c6837
Parth Mahajan <parthmahajan@gmail.com>
2017-05-22 21:04:17 +0530

    - added ctrls

---
+++ features/user/controller/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+exports.getUserCtrl = function(opts){
+	try{
+
+		console.log('get user data ctrl');
+	} catch(err){
+
+		console.log('File: User, controller, Error: ', err);
+		throw(err);
+	}
+}
+
+exports.addUserCtrl = function(opts){
+	try{
+
+		console.log('add user data ctrl');
+	} catch(err){
+
+		console.log('File: User, controller, Error: ', err);
+		throw(err);
+	}
+}
